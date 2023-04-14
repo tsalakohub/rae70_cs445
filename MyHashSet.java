285f51d022a397a946ef50d17dbe0cc1335e87e8
r3xley <rexley623@gmail.com>
2023-04-14 10:09:02 -0400

    cahnge

--- MyHashSet.java
+++ MyHashSet.java
@@ -19,8 +19,62 @@ public class MyHashSet implements HS_Interface {
                 numBuckets, MAX_ACCEPTABLE_AVE_BUCKET_SIZE);
     }
 
+    public int hashOf(String key) // h MUST BE IN [0..numBuckets-1]
+    {
+        int total = 0;
+        int power = 1;
+        for (int i = 0; i < key.length(); i++) {
+
+            int n = key.charAt(i) - 96;
+
+            total += n * power;
+            power *= 29;
+            power %= numBuckets;
+            total %= numBuckets;
+
+        }
+        return Math.abs(total) % numBuckets;
+    }
+
+    public Node search(String key) {
+
+        Node cur = bucketArray[hashOf(key)];
+        while (cur != null && !key.equals(cur.data)) {
+            cur = cur.next;
+        }
+        return cur; // YOUR CODE HERE
+    }
+
+    public boolean removeAtFront(String key) // RETURNS TRUE IF THERE WAS NODE TO REMOVE
+    {
+        if (bucketArray[hashOf(key)] == null) {
+            return false;
+        }
+        bucketArray[hashOf(key)] = bucketArray[hashOf(key)].next;
+        size--;
+        return true;
+
+        // YOUR CODE HERE
+    }
+
     public boolean add(String key) {
         // your code here to add the key to the table and ++ your size variable
+        int index = hashOf(key);
+        Node cur = bucketArray[index];
+        if (cur == null) {
+            bucketArray[index] = new Node(key, null);
+            size++;
+            return true;
+        }
+        while (cur.next != null) {
+            if (cur.data.equals(key)) {
+                return false;
+
+            }
+            cur = cur.next;
+
+        }
+        cur.next = new Node(key, null);
 
         ++size; // you just added a key to one of the lists
         if (size > MAX_ACCEPTABLE_AVE_BUCKET_SIZE * this.numBuckets)
@@ -29,7 +83,8 @@ public class MyHashSet implements HS_Interface {
     }
 
     public boolean contains(String key) {
-        return false; // just to make it compile.
+        return search(key) != null;
+        // just to make it compile.
         // You hash this key. goto that list. look for this key in that list
     }
 
@@ -42,22 +97,61 @@ public class MyHashSet implements HS_Interface {
         /*
          * FOR EACH LIST IN THE ARRAY
          * FOR EACH NODE IN THAT LIST
+         * 
          * HASH THAT KEY INTO THE BIGGER TABLE
          * BE SURE TO USE THE BIGGER .LENGTH AS THE MODULUS
+         * 
          */
+
+        for (int i = 0; i < bucketArray.length; i++) {
+            Node cur = bucketArray[i];
+            while (cur.next != null) {
+                int index = hashOf(cur.data);
+                Node temp = biggerArray[index];
+                if (temp == null) {
+                    biggerArray[index] = new Node(cur.data, null);
+                } else {
+                    while (temp.next != null) {
+                        if (temp.data.equals(cur.data)) {
+                            break;
+                        }
+                        temp = temp.next;
+                    }
+                    if (temp.next == null) {
+                        temp.next = new Node(cur.data, null);
+                    }
+                }
+                cur = cur.next;
+            }
+
+        }
         bucketArray = biggerArray;
     } // END OF UPSIZE & REHASH
 
     public boolean remove(String key) {
-        return false;
+        if (bucketArray[hashOf(key)] == null || bucketArray[hashOf(key)].data.equals(key)) {
+            return removeAtFront(key);
+        }
+
+        Node cur = bucketArray[hashOf(key)];
+        while (cur.next != null && !cur.next.data.equals(key)) {
+            cur = cur.next;
+        }
+        if (cur.next == null) {
+            return false;
+        } else {
+            cur.next = cur.next.next;
+            size--;
+            return true;
+        }
     }
 
     public boolean isEmpty() {
-        return(size() == 0);
+        return size() == 0;
     }
 
     public void clear() {
-        for(int i = 0; i < bucketArray.length; i++){
+        for (int i = 0; i < bucketArray.length; i++) {
             bucketArray[i] = null;
         }
     }
